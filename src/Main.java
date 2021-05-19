14e743b2787308b4cf913149f58382792195b738
Micael Fischmann <micael.fischmann@gmail.com>
2021-05-19 19:17:02 -0300

    Adequa autorizacao e exames

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,23 @@
 import model.Medico;
+import model.Autorizacoes;
 import model.Exames;
 ;
-
 
 public class Main {
 
     public static void main(String[]args){
 
-        Exames exames = new Exames();
         //adiciona os exames disponíveis na clínica
-        exames.examesDisponiveis();
         
         //PARA EXEMPLO
         //Medico medico = new Medico("123","Luis", "Pereira", "123456");
 
     }
+
+    public void examesDisponiveis(){
+        for (Exames exame : Exames.values()) {
+            System.out.println(exame);
+          }
+    } 
 
 }
