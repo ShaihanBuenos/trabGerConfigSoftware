a1fa59c8860f6ab8fda8cd0dcbb4e05c55ca8b79
Micael Fischmann <micael.fischmann@gmail.com>
2021-05-19 18:46:21 -0300

    Adequa Classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import model.Medico;
+import model.Paciente;
+import model.Administrador;
 import model.Exames;
-;
 
 
 public class Main {
@@ -13,7 +14,13 @@ public class Main {
         
         //PARA EXEMPLO
         //Medico medico = new Medico("123","Luis", "Pereira", "123456");
+        Medico        medico1 = new Medico       ("Lucas", "Cardoso", "194528-RS");
+        Administrador adm1    = new Administrador("Paulo", "Soarez");
+        Paciente      pac1    = new Paciente     ("Joana", "Silva", "JSilva@gmail.com");
 
+        System.out.println(medico1.toString());
+        System.out.println(adm1.toString());
+        System.out.println(pac1.toString());
     }
 
 }
